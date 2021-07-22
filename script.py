11e962b294f22ffb484a00ce1c030c853c817570
ara <romaric.farenc@gmail.com>
2021-07-22 10:46:49 +0200

    added selectbox and comparison by continent

--- script.py
+++ script.py
@@ -1,26 +1,78 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-# st.title('Hello Wilders, welcome to my application!')
+#st.set_page_config(layout="wide")
 
-# st.write("I enjoy to discover stremalit possibilities")
+URL_CSV = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
+df = pd.read_csv(URL_CSV)
+df.continent = df.continent.apply( lambda c: c[0:-1] )
 
-# link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-# df_weather = pd.read_csv(link)
-# st.write(df_weather)
+ALL_CONTINENT = 'All continents'
+regions = [ALL_CONTINENT]
+regions.extend( list(df.continent.unique()) )
 
-# st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+col_title, col_cont, _ = st.beta_columns((5, 5, 4))
+with col_title:
+  st.title('Car analysis from')
+with col_cont:
+  selected_region = st.selectbox('', regions )
 
-# viz_correlation = sns.heatmap(df_weather.corr(), 
-# 								center=0,
-# 								cmap = sns.color_palette("vlag", as_cmap=True)
-# 								)
+col1, col2 = st.beta_columns([5,3])
 
-# st.pyplot(viz_correlation.figure)
+with st.beta_container():
 
-st.title('Hello Wilders, welcome to my application!')
+  with col2:
+    st.header('Heatmap of car attributes')
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+    viz = sns.heatmap(
+      df.corr(),
+      cmap= 'inferno',
+      center= 0
+    )
+    st.write(viz.figure)
+    
+    st.write('We can see strong correlations all over the place 🍾🥂')
+
+  with col1:
+    st.header(f'Weight per horse power')
+    
+    df_continent = df if selected_region == ALL_CONTINENT else df[ df.continent == selected_region ]
+
+    ax = plt.subplot()
+    viz_bar = sns.scatterplot(
+      data= df_continent,
+      ax= ax,
+      y= 'weightlbs',
+      x= 'hp',
+      hue= 'time-to-60',
+      size= 'cubicinches',
+      palette= 'inferno')
+    plt.legend( bbox_to_anchor= (1, 1) )
+
+    viz_bar.set_xlabel('Horse power')
+    viz_bar.set_ylabel('Weight (lbs)')
+
+    st.write( viz_bar.figure )
+
+df_cont = df[['weightlbs', 'hp', 'time-to-60', 'cubicinches', 'continent']]\
+  .groupby('continent')\
+  .agg('mean')
+
+df_cont.rename( columns= { 'hp': 'Horse Power' }, inplace= True )
+
+
+with st.beta_container():
+  st.title( 'Comparisons by continent' )
+  col3, col4, col5 = st.beta_columns(3)
+  
+  with col3:
+    st.bar_chart( df_cont[['Horse Power']] )
+
+  with col4:
+    st.bar_chart( df[['cylinders', 'continent']].groupby('continent').agg('median') )
+
+  with col5:
+    st.bar_chart( df_cont[['time-to-60']] )
